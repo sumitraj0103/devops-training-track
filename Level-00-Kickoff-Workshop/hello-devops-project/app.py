b3d8a79b3c0d9e597e1fa53c2483b584148db7a6
Sumit <sumit.raj103103@gmail.com>
2025-02-21 08:05:55 +0300

    Update app.py

--- Level-00-Kickoff-Workshop/hello-devops-project/app.py
+++ Level-00-Kickoff-Workshop/hello-devops-project/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello DevOps! My Name is Sumit"
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
